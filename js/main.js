b62a070a52dd466fd988f9d4324b53482083c8c3
Spud97 <luca.dercole97@gmail.com>
2022-05-17 15:40:16 +0200

    correzione

--- js/main.js
+++ js/main.js
@@ -5,5 +5,5 @@ const coloreUtente = prompt("Colore preferito")
 console.log(nomeUtente, cognomeUtente, coloreUtente);
 // NICKNAME
 const spanNickName = document.getElementById("nickname");
-spanNickName.innerHTML = ${nomeUtente} ${cognomeUtente} ${coloreUtente}<span>22</span>;
+spanNickName.innerHTML = "${nomeUtente} ${cognomeUtente} ${coloreUtente}"<span>"22"</span>;
 console.dir(spanNickName);
