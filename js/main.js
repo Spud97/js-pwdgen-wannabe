cf98c86b4fc8e6e5f00c8f56ddbb8021c0875b30
Spud97 <luca.dercole97@gmail.com>
2022-05-17 15:33:14 +0200

    ticket

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@
 const nomeUtente = prompt("Nome");
 const cognomeUtente = prompt("Cognome");
 const coloreUtente = prompt("Colore preferito")
+console.log(nomeUtente, cognomeUtente, coloreUtente);
 // NICKNAME
 const spanNickName = document.getElementById("nickname");
-spanNickName.innerHTML = ${nomeUtente} ${cognomeUtente} ${coloreUtente}
-
+spanNickName.innerHTML = ${nomeUtente} ${cognomeUtente} ${coloreUtente}<span>22</span>;
+console.dir(spanNickName);
