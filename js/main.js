41334e8c151d23aa88abe0d79eba4469e887a7ff
Spud97 <luca.dercole97@gmail.com>
2022-05-17 15:18:51 +0200

    aggiunto titolo

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+// RICHIESTE
 const nomeUtente = prompt("Nome");
 const cognomeUtente = prompt("Cognome");
 const coloreUtente = prompt("Colore preferito")
+// NICKNAME
+const spanNickName = document.getElementById("nickname");
+spanNickName.innerHTML = ${nomeUtente} ${cognomeUtente} ${coloreUtente}
+
